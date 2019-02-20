d37e4796e833966f2b289eabd68d3ea26873e142
Erick Ghaumez <rxlabz@gmail.com>
2019-02-20 01:22:41 +0100

    refacto

--- lib/color_scheme_controls.dart
+++ lib/color_scheme_controls.dart
@@ -12,81 +12,99 @@ class ColorSchemeControls extends StatefulWidget {
   _ColorSchemeControlsState createState() => _ColorSchemeControlsState();
 }
 
+class ColorLabel {
+  final String label;
+  final Color value;
+
+  const ColorLabel(this.label, this.value);
+}
+
+class ColorSelectorData {
+  final ColorLabel color;
+  final ValueChanged<Color> onColorChange;
+
+  const ColorSelectorData(this.color, this.onColorChange);
+}
+
 class _ColorSchemeControlsState extends State<ColorSchemeControls> {
+  List<ColorLabel> primaries;
+  List<ColorLabel> secondaries;
+  List<ColorLabel> onMainColors;
+  List<ColorLabel> backgroundColors;
+  List<ColorLabel> surfaceColors;
+  List<ColorLabel> errorColors;
+
   @override
   void initState() {
     super.initState();
+
+    _initColors();
   }
 
-  onColorSelected(String property, Color value) {
-    widget.onSchemeChanged(apply(property, value));
+  @override
+  void didUpdateWidget(ColorSchemeControls oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.scheme != widget.scheme) _initColors();
   }
 
-  ColorScheme apply(String colorLabel, Color color) {
+  void _initColors() {
+    primaries = [
+      ColorLabel('primary', widget.scheme.primary),
+      ColorLabel('primaryVariant', widget.scheme.primaryVariant),
+    ];
+    secondaries = [
+      ColorLabel('secondary', widget.scheme.secondary),
+      ColorLabel('secondaryVariant', widget.scheme.secondaryVariant),
+    ];
+
+    onMainColors = [
+      ColorLabel('onPrimary', widget.scheme.onPrimary),
+      ColorLabel('onSecondary', widget.scheme.onSecondary),
+    ];
+
+    backgroundColors = [
+      ColorLabel('background', widget.scheme.background),
+      ColorLabel('onBackground', widget.scheme.onBackground),
+    ];
+
+    surfaceColors = [
+      ColorLabel('surface', widget.scheme.surface),
+      ColorLabel('onSurface', widget.scheme.onSurface),
+    ];
+
+    errorColors = [
+      ColorLabel('error', widget.scheme.error),
+      ColorLabel('onError', widget.scheme.onError),
+    ];
+  }
+
+  void _onColorSelected(String property, Color value) =>
+      widget.onSchemeChanged(_apply(property, value));
+
+  ColorScheme _apply(String colorLabel, Color color) {
     Map<Symbol, dynamic> args = {Symbol(colorLabel): color};
     return Function.apply(widget.scheme.copyWith, null, args);
   }
 
-  ColorSelectorRow _buildColorSelectorRow(List<List<dynamic>> colors) =>
+  ColorSelectorRow _buildColorSelectorRow(List<ColorLabel> colors) =>
       ColorSelectorRow(
         colors: colors
-            .map((item) => ColorSelectorData(
-                item[0], item[1], (c) => onColorSelected(item[0], c)))
+            .map((item) =>
+                ColorSelectorData(item, (c) => _onColorSelected(item.label, c)))
             .toList(),
       );
 
   @override
-  Widget build(BuildContext context) {
-    final List<List<dynamic>> primaries = [
-      ['primary', widget.scheme.primary],
-      ['primaryVariant', widget.scheme.primaryVariant],
-    ];
-
-    final secondaries = [
-      ['secondary', widget.scheme.secondary],
-      ['secondaryVariant', widget.scheme.secondaryVariant],
-    ];
-
-    final onMainColors = [
-      ['onPrimary', widget.scheme.onPrimary],
-      ['onSecondary', widget.scheme.onSecondary],
-    ];
-
-    final backgroundColors = [
-      ['background', widget.scheme.background],
-      ['onBackground', widget.scheme.onBackground],
-    ];
-
-    final surfaceColors = [
-      ['surface', widget.scheme.surface],
-      ['onSurface', widget.scheme.onSurface],
-    ];
-
-    final errorColors = [
-      ['error', widget.scheme.error],
-      ['onError', widget.scheme.onError],
-    ];
-
-    return ListView(
-      children: <Widget>[
-        _buildColorSelectorRow(primaries),
-        _buildColorSelectorRow(secondaries),
-        _buildColorSelectorRow(onMainColors),
-        _buildColorSelectorRow(backgroundColors),
-        _buildColorSelectorRow(surfaceColors),
-        _buildColorSelectorRow(errorColors),
-      ],
-    );
-  }
-}
-
-class ColorSelectorData {
-  final Color color;
-  final ValueChanged<Color> onColorChange;
-
-  final String label;
-
-  const ColorSelectorData(this.label, this.color, this.onColorChange);
+  Widget build(BuildContext context) => ListView(
+        children: <Widget>[
+          _buildColorSelectorRow(primaries),
+          _buildColorSelectorRow(secondaries),
+          _buildColorSelectorRow(onMainColors),
+          _buildColorSelectorRow(backgroundColors),
+          _buildColorSelectorRow(surfaceColors),
+          _buildColorSelectorRow(errorColors),
+        ],
+      );
 }
 
 class ColorSelectorRow extends StatelessWidget {
@@ -95,11 +113,11 @@ class ColorSelectorRow extends StatelessWidget {
   const ColorSelectorRow({Key key, this.colors}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Row(children: colors.map(_buildColorBox).toList());
-  }
+  Widget build(BuildContext context) =>
+      Row(children: colors.map(_buildColorBox).toList());
 
-  Widget _buildColorBox(ColorSelectorData color) => Flexible(
-        child: ColorSelector(color.label, color.color, color.onColorChange),
+  Widget _buildColorBox(ColorSelectorData data) => Flexible(
+        child: ColorSelector(
+            data.color.label, data.color.value, data.onColorChange),
       );
 }
